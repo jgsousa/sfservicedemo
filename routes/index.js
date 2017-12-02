9dee0c53a8275dade32c0a78cc40fa9e076db6c1
jgsousa <asousa.joao@gmail.com>
2017-12-02 19:01:34 +0000

    Rest API

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var booking1 = {
+    "reservation" : "BNK123",
+    "lastname" : "Marto",
+    "firstname" : "Cesar",
+    "passengers" : [
+        { "name" : "Cesar Marto" , "email" : "joasousa@deloite.pt" },
+        { "name" : "Sonia Marto", "email" : "example@example.com"}
+    ],
+    "segments" : [
+        { "flightNumber" : "TP130", "origin" : "LIS", "destination" : "GVA" }
+    ]
+};
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -13,6 +26,17 @@ router.get('/prechat', function(req, res, next) {
 router.get('/thankyou', function(req, res, next) {
     res.render('thankyou', {});
 });
+
+router.get('/booking/:id', function(req, res, next) {
+    var id = req.params.id;
+    if(id === 'BNK123'){
+        res.json({ "status": "OK", "booking" : booking1 });
+    }
+    else{
+        res.status(500).send({ error: 'Not found!' })
+    }
+});
+
 
 
 module.exports = router;
